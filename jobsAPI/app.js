8e921617df4cbc8bfc60ceb0af49bd3dfeb5a4a6
Andrei Popovici <popovici.andrey@gmail.com>
2023-02-09 01:33:44 +0200

    add swagger to prod

--- jobsAPI/app.js
+++ jobsAPI/app.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -35,7 +40,11 @@ app.use(cors());
 app.use(xss());
 
 //routes
-app.get('/', (req, res) => res.send(`<h1>Jobs API</h1>`));
+app.get('/', (req, res) =>
+    res.send(`<h1>Jobs API</h1><a href="/api-docs">Documentation</a>`)
+);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
 app.use('/api/v1/auth', authRoute);
 app.use('/api/v1/jobs', authenticationMW, jobsRoute);
 
